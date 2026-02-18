4c9f54745f67805c67493c73b146ec0589b0640e
cesarcarpizo-oss <cesar_carpizo@dlsu.edu.ph>
2026-02-18 09:52:36 +0000

    First Practices

---
+++ Variables.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h> 
+
+int main()
+{
+
+int age = 19;
+float gpa = 3.5;
+char grade = 'A';
+
+printf ("What is your age: %d\n", age);
+printf ("What is your gpa: %f\n", gpa);
+printf ("What is your geder: %c\n", grade);
+
+
+
+return 0;
+}
+
